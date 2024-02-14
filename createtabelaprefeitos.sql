d24a8ec5eae4a9cd22ac046d0b971bda622017b7
brenoob <breno_1993@outlook.com>
2024-02-14 11:19:16 -0300

    tabelas inserts selects  e join

--- createtabelaprefeitos.sql
+++ createtabelaprefeitos.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS prefeitos (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL UNIQUE,
-    cidade_id BIGINT NOT NULL,
+    cidade_id BIGINT,
     FOREIGN KEY (cidade_id) REFERENCES cidades(id)
 );
---
+++ inserirprefeitos.sql
@@ -0,0 +1,8 @@
+select * from cidades
+
+insert into prefeitos (nome, cidade_id)
+VALUES
+('Dário Saadi', 1),
+('João Campos', 9),
+('Rodrigo Pinheiro', 7),
+('Axel Grael', 2)
